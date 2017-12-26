fc1cce6c4b3ca5711c924c3a15a4f977b6f33a8b
daiqi5527153 <466608943@qq.com>
2017-12-26 17:26:47 +0800

    分布式配置测试客户端

--- dq-easy-cloud-config-client/src/main/java/com/dq/easy/cloud/DqEasyCloudConfigClientApplication.java
+++ dq-easy-cloud-config-client/src/main/java/com/dq/easy/cloud/DqEasyCloudConfigClientApplication.java
@@ -3,8 +3,6 @@ package com.dq.easy.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-
 @SpringBootApplication
 //Eureka客户端
 @EnableDiscoveryClient
