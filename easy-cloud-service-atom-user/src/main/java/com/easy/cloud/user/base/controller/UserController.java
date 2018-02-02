73fddc4ad438d692747331174b5cf15604bf19b5
daiqi <466608943@qq.com>
2018-02-02 14:26:51 +0800

    添加用户和电影服务的分布式配置

--- easy-cloud-service-atom-user/src/main/java/com/easy/cloud/user/base/controller/UserController.java
+++ easy-cloud-service-atom-user/src/main/java/com/easy/cloud/user/base/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.easy.cloud.user.base.controller;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dq.easy.cloud.model.common.json.utils.DqJSONUtils;
 import com.dq.easy.cloud.model.common.response.vo.DqResponseVO;
 import com.easy.cloud.user.base.entity.UserEntity;
 import com.easy.cloud.user.base.query.UserQuery;
@@ -16,7 +22,15 @@ import com.easy.cloud.user.base.service.inf.UserService;
 public class UserController{
 	@Autowired
 	private UserService userService;
+	@Value(value="${from}")
+	private String from;
 	
+	@RequestMapping("getFrom")
+	public String getFrom(){
+		Map<String, Object> retMap = new HashMap<>();
+		retMap.put("from", from);
+		return DqJSONUtils.parseObject(retMap, String.class);
+	}
 	@RequestMapping("getUserInfoById")
 	public String getUserInfoById(@RequestParam Long id){
 		UserEntity userEntity = userService.findUserById(id);
