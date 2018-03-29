cb1cc0fb4b4fdd71af24b617227bb86d41cbb3e3
daiqi <466608943@qq.com>
2018-03-29 11:32:43 +0800

    修复service目录结构的问题

--- dq-easy-cloud-parent/dq-easy-cloud-common/src/main/java/com/dq/easy/cloud/module/common/generator/code/xml/constant/DqCodeGenerateXmlConstant.java
+++ dq-easy-cloud-parent/dq-easy-cloud-common/src/main/java/com/dq/easy/cloud/module/common/generator/code/xml/constant/DqCodeGenerateXmlConstant.java
@@ -11,5 +11,13 @@ package com.dq.easy.cloud.module.common.generator.code.xml.constant;
  * 创建时间    2018年3月28日 下午2:49:17
  */
 public class DqCodeGenerateXmlConstant {
+	/** xml申明类 */
+	public static class DqStatement {
+		public static final String DEFAULT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+	}
 	
+	/** xml文档类型常量类 */
+	public static class DqDocType {
+		public static final String MYBATIS = "<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd\">";
+	}
 }
