b4da5a435f3b14dc1beef2cd45708891ccbad18a
daiqi <466608943@qq.com>
2018-03-29 11:00:10 +0800

    增加xml

---
+++ dq-easy-cloud-parent/dq-easy-cloud-common/src/main/java/com/dq/easy/cloud/module/common/generator/code/xml/desc/DqXmlContentBaseDesc.java
@@ -0,0 +1,26 @@
+package com.dq.easy.cloud.module.common.generator.code.xml.desc;
+
+import com.dq.easy.cloud.module.common.file.pojo.desc.DqFileContentBaseDesc;
+import com.dq.easy.cloud.module.common.generator.code.base.sources.database.DqDatabaseDataSources;
+
+/**
+ * 
+ * <p>
+ * xml内容基础描述类
+ * </p>
+ *
+ * <pre>
+ *  说明：所有xml描述类的最终父类
+ * </pre>
+ *
+ * @author daiqi
+ * 创建时间    2018年3月29日 上午10:30:14
+ */
+public class DqXmlContentBaseDesc extends DqFileContentBaseDesc{
+
+	@Override
+	public DqFileContentBaseDesc buildDataByDatabaseSources(DqDatabaseDataSources databaseDataSources) {
+		return null;
+	}
+
+}
