a6d3322143eacdcc7f5e8a4e7fa8e33c6420c32d
daiqi <466608943@qq.com>
2018-05-03 09:41:23 +0800

    添加spplication配置内容

--- easy-cloud-core-parent/easy-cloud-core-cache/src/main/java/com/easy/cloud/core/cache/EcCoreCacheApplication.java
+++ easy-cloud-core-parent/easy-cloud-core-cache/src/main/java/com/easy/cloud/core/cache/EcCoreCacheApplication.java
@@ -1,13 +1,16 @@
 package com.easy.cloud.core.cache;
 
-/**
- * Hello world!
- *
- */
-public class EcCoreCacheApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
+public class EcCoreCacheApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EcCoreCacheApplication.class, args);
+	}
 }
+
--- easy-cloud-core-parent/easy-cloud-core-lock/src/main/java/com/easy/cloud/core/lock/EcCoreLockApplication.java
+++ easy-cloud-core-parent/easy-cloud-core-lock/src/main/java/com/easy/cloud/core/lock/EcCoreLockApplication.java
@@ -1,13 +1,20 @@
 package com.easy.cloud.core.lock;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
+
+
 /**
  * Hello world!
  *
  */
-public class EcCoreLockApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+@ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
+public class EcCoreLockApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EcCoreLockApplication.class, args);
+	}
 }
