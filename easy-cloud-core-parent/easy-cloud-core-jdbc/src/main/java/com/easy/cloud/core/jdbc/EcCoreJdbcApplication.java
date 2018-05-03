5480604d3ce6daf1a2358274f7b707341f82e89a
daiqi <466608943@qq.com>
2018-05-03 09:41:23 +0800

    添加spplication配置内容

--- easy-cloud-core-parent/easy-cloud-core-jdbc/src/main/java/com/easy/cloud/core/jdbc/EcCoreJdbcApplication.java
+++ easy-cloud-core-parent/easy-cloud-core-jdbc/src/main/java/com/easy/cloud/core/jdbc/EcCoreJdbcApplication.java
@@ -1,13 +1,18 @@
 package com.easy.cloud.core.jdbc;
 
-/**
- * Hello world!
- *
- */
-public class EcCoreJdbcApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
+public class EcCoreJdbcApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EcCoreJdbcApplication.class, args);
+	}
 }
--- easy-cloud-core-parent/easy-cloud-core-mq/src/main/java/com/easy/cloud/core/mq/EcCoreMqApplication.java
+++ easy-cloud-core-parent/easy-cloud-core-mq/src/main/java/com/easy/cloud/core/mq/EcCoreMqApplication.java
@@ -1,13 +1,20 @@
 package com.easy.cloud.core.mq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
+
+
 /**
- * Hello world!
+ * EcCoreMqApplication
  *
  */
-public class EcCoreMqApplication 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+@ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
+public class EcCoreMqApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EcCoreMqApplication.class, args);
+	}
 }
