fc9d3fa59371d2d35e941bdf8e296e143f78a0ed
daiqi <466608943@qq.com>
2018-05-04 11:38:44 +0800

    锁添加配置文件

--- easy-cloud-core-parent/easy-cloud-core-mq/src/main/java/com/easy/cloud/core/mq/EcCoreMqApplication.java
+++ easy-cloud-core-parent/easy-cloud-core-mq/src/main/java/com/easy/cloud/core/mq/EcCoreMqApplication.java
@@ -2,16 +2,18 @@ package com.easy.cloud.core.mq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
 
 
 /**
- * EcCoreMqApplication
- *
+ * 
+ * 不自动注入数据源
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
 public class EcCoreMqApplication {
 	public static void main(String[] args) {
