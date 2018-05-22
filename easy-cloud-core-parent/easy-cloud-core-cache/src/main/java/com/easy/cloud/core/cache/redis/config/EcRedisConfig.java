4634e01207529da472c793e75a95e816b22a1885
daiqi <466608943@qq.com>
2018-05-22 11:33:51 +0800

    优化锁结构

--- easy-cloud-core-parent/easy-cloud-core-cache/src/main/java/com/easy/cloud/core/cache/redis/config/EcRedisConfig.java
+++ easy-cloud-core-parent/easy-cloud-core-cache/src/main/java/com/easy/cloud/core/cache/redis/config/EcRedisConfig.java
@@ -1,10 +1,6 @@
 package com.easy.cloud.core.cache.redis.config;
 
-import java.lang.reflect.Method;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
@@ -18,22 +14,7 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
  */
 public class EcRedisConfig {
 	@Autowired
-	private EcRedisProperties redisProperties;
-	@Bean
-	public KeyGenerator keyGenerator() {
-		return new KeyGenerator() {
-			@Override
-			public Object generate(Object target, Method method, Object... params) {
-				StringBuilder sb = new StringBuilder();
-				sb.append(target.getClass().getName());
-				sb.append(method.getName());
-				for (Object obj : params) {
-					sb.append(obj.toString());
-				}
-				return sb.toString();
-			}
-		};
-	}
+	protected EcRedisProperties redisProperties;
 
 	/**
 	 * 创建连接
