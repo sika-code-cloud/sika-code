2295895ffa19d6c65b2c673add243431c2fc05c6
daiqi <466608943@qq.com>
2018-05-22 14:15:15 +0800

    设置

--- easy-cloud-core-parent/easy-cloud-core-lock/src/main/java/com/easy/cloud/core/lock/EcCoreLockApplication.java
+++ easy-cloud-core-parent/easy-cloud-core-lock/src/main/java/com/easy/cloud/core/lock/EcCoreLockApplication.java
@@ -2,8 +2,6 @@ package com.easy.cloud.core.lock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
@@ -13,7 +11,7 @@ import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
  * 不自动注入数据源
  *
  */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
 public class EcCoreLockApplication {
 	public static void main(String[] args) {
--- easy-cloud-core-parent/easy-cloud-core-search/src/main/java/com/easy/cloud/core/search/EcCoreSearchApplication.java
+++ easy-cloud-core-parent/easy-cloud-core-search/src/main/java/com/easy/cloud/core/search/EcCoreSearchApplication.java
@@ -2,8 +2,6 @@ package com.easy.cloud.core.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
@@ -11,11 +9,10 @@ import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
 
 /**
  * 
- * 不自动注入数据源
  * @author daiqi
  * @date 2018年3月18日 上午12:47:25
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@SpringBootApplication
 @ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
 public class EcCoreSearchApplication {
 	public static void main(String[] args) {
