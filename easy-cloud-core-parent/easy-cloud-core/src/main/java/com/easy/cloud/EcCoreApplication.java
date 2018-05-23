cf71d6350eee1ef870381973df18770dd3680f31
daiqi <466608943@qq.com>
2018-05-23 11:02:15 +0800

    提交servlet容器销毁执行的业务逻辑

--- easy-cloud-core-parent/easy-cloud-core/src/main/java/com/easy/cloud/EcCoreApplication.java
+++ easy-cloud-core-parent/easy-cloud-core/src/main/java/com/easy/cloud/EcCoreApplication.java
@@ -2,6 +2,7 @@ package com.easy.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
@@ -13,6 +14,8 @@ import com.easy.cloud.core.basic.constant.EcBaseComponentScan;
  * @date 2018年3月18日 上午12:47:25
  */
 @SpringBootApplication
+// 扫描ServletContextListener的监听器
+@ServletComponentScan
 @ComponentScan(basePackages = { EcBaseComponentScan.COM_EASY_CLOUD})
 public class EcCoreApplication {
 	public static void main(String[] args) {
