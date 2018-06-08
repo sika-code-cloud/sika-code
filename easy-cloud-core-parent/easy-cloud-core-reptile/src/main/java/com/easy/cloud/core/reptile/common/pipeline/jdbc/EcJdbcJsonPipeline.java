812ef285da17cc9c0260acbf9792f4c46256c5ae
daiqi <466608943@qq.com>
2018-06-08 18:42:01 +0800

    修复

--- easy-cloud-core-parent/easy-cloud-core-reptile/src/main/java/com/easy/cloud/core/reptile/common/pipeline/jdbc/EcJdbcJsonPipeline.java
+++ easy-cloud-core-parent/easy-cloud-core-reptile/src/main/java/com/easy/cloud/core/reptile/common/pipeline/jdbc/EcJdbcJsonPipeline.java
@@ -1,12 +1,18 @@
 package com.easy.cloud.core.reptile.common.pipeline.jdbc;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.alibaba.fastjson.JSONObject;
 import com.easy.cloud.core.reptile.common.pipeline.EcBaseJsonPipeline;
+import com.easy.cloud.core.reptile.datafield.service.EcReptileDataFieldService;
 import com.geccocrawler.gecco.annotation.PipelineName;
 
 @PipelineName(value = "jdbcJsonPipeline")
 public class EcJdbcJsonPipeline extends EcBaseJsonPipeline{
-
+	
 	@Override
 	public void process(JSONObject jo) {
 		System.out.println(Thread.currentThread().getName() + "---jdbc获取到的数据：" + JSONObject.toJSONString(jo));
