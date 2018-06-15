0e7fd96e11c3c1eb04d675807880a1dfc361815d
daiqi <466608943@qq.com>
2018-06-15 14:36:19 +0800

    晚上quartz

--- easy-cloud-core-parent/easy-cloud-core-jdbc/src/main/java/com/easy/cloud/core/jdbc/test/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.easy.cloud.core.jdbc.test.dao;
-
-import com.easy.cloud.core.jdbc.base.dao.EcBaseDAO;
-import com.easy.cloud.core.jdbc.test.UserEntity;
-public interface UserDAO extends EcBaseDAO<UserEntity>{
-
-}
