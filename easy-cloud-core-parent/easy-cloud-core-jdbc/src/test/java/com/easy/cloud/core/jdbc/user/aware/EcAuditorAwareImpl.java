72916d26cb47101c0ae2f8c6b645a35f35100f39
daiqi <466608943@qq.com>
2018-07-13 23:00:44 +0800

    清除无用类

--- easy-cloud-core-parent/easy-cloud-core-jdbc/src/test/java/com/easy/cloud/core/jdbc/user/aware/EcAuditorAwareImpl.java
+++ easy-cloud-core-parent/easy-cloud-core-jdbc/src/test/java/com/easy/cloud/core/jdbc/user/aware/EcAuditorAwareImpl.java
@@ -1,8 +1,7 @@
 package com.easy.cloud.core.jdbc.user.aware;
 
-import org.springframework.stereotype.Component;
-
 import com.easy.cloud.core.jdbc.audit.aware.EcAuditorAware;
+import org.springframework.stereotype.Component;
 
 @Component
 public class EcAuditorAwareImpl implements EcAuditorAware<Long>{
