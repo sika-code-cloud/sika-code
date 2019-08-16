b6fde57eef9f4a4fd7e5e27ab8d8e8b88d66ab52
daiqi <466608943@qq.com>
2019-08-16 23:50:59 +0800

    添加分布式锁组件示例

--- sika-code-core/standard-footer-demo/standard-footer-demo-core/src/main/java/com/sika/code/standard/footer/demo/business/accessruletype/logic/impl/AccessRuleTypeLogicImpl.java
+++ sika-code-core/standard-footer-demo/standard-footer-demo-core/src/main/java/com/sika/code/standard/footer/demo/business/accessruletype/logic/impl/AccessRuleTypeLogicImpl.java
@@ -1,9 +1,11 @@
 package com.sika.code.standard.footer.demo.business.accessruletype.logic.impl;
 
 
+import com.google.common.collect.Lists;
+import com.sika.code.cache.redis.util.RedisUtil;
 import com.sika.code.lock.anotation.DistributionLock;
-import com.sika.code.lock.constant.LockType;
 import com.sika.code.lock.distribution.DistributionLockHandler;
+import com.sika.code.lock.pojo.result.LockResult;
 import com.sika.code.lock.properties.DistributionLockProperties;
 import com.sika.code.standard.base.logic.BaseStandardLogic;
 import com.sika.code.standard.footer.demo.business.accessruletype.logic.AccessRuleTypeLogic;
@@ -23,6 +25,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
  * <p>
  * 准入规则类型表 逻辑实现类
@@ -35,8 +40,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccessRuleTypeLogicImpl extends BaseStandardLogic implements AccessRuleTypeLogic {
     @Autowired
     private AccessRuleTypeService accessRuleTypeService;
-    @Autowired
-    private DistributionLockHandler distributionLockHandler;
     @Autowired
     private DistributionLockProperties distributionLockProperties;
 
@@ -68,29 +71,76 @@ public class AccessRuleTypeLogicImpl extends BaseStandardLogic implements Access
         return request.execute();
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     @Override
-    @DistributionLock(modules = {"user", "order"}, fieldName = "queryRequest.accessRuleTypeId")
+    @DistributionLock(modules = {"user", "order"}, fieldName = "ueryRequest.accessRuleTypeId")
     public AccessRuleTypeListQueryResponseBO list(AccessRuleTypeListQueryRequestBO request) {
         return request.execute();
     }
 
+    @Autowired
+    private DistributionLockHandler distributionLockHandler;
 
+    /**
+     * <p>
+     * 锁示例 --- 具体使用选择一种锁即可
+     * </p>
+     */
+    public AccessRuleTypeListQueryResponseBO lock(AccessRuleTypeListQueryRequestBO request) {
+        String typeIdStr = String.valueOf(request.getQueryRequest().getAccessRuleTypeId());
+        String lockKey = RedisUtil.generateRedisKey("user", "order", typeIdStr);
 
+        // 级联key的列表
+        List<String> lockKeys = Lists.newArrayList();
+        for (String typeId : request.getQueryRequest().getAccessRuleTypeIds()) {
+            lockKeys.add(RedisUtil.generateRedisKey("user", "order", typeId));
+        }
+        LockResult lockResult = null;
+        try {
+            // 1:非公平锁
+            // 1.1:无过期时间的锁
+            lockResult = distributionLockHandler.lock(lockKey);
+            // 1.2:有过期时间的锁 key的过期时间为5秒
+            lockResult = distributionLockHandler.lock(lockKey, 5);
+            // 1.3:带时间单位的锁 key的过期时间为5000毫秒
+            lockResult = distributionLockHandler.lock(lockKey, 5000, TimeUnit.MILLISECONDS);
+            // --- 级联锁 ---
+            // 1.4:无过期时间的锁
+            lockResult = distributionLockHandler.lock(lockKeys);
+            // 1.5:有过期时间的锁 key的过期时间为5秒
+            lockResult = distributionLockHandler.lock(lockKeys, 5);
+            // 1.6:带时间单位的锁 key的过期时间为5000毫秒
+            lockResult = distributionLockHandler.lock(lockKeys, 5000, TimeUnit.MILLISECONDS);
 
+            // 2:公平锁
+            // 2.1:无过期时间的锁
+            lockResult = distributionLockHandler.fairLock(lockKey);
+            // 2.2:有过期时间的锁 key的过期时间为5秒
+            lockResult = distributionLockHandler.fairLock(lockKey,5 );
+            // 2.3:带时间单位的锁 key的过期时间为5000毫秒
+            lockResult = distributionLockHandler.fairLock(lockKey, 5000, TimeUnit.MILLISECONDS);
 
+            // 3:尝试非公平锁锁
+            // 3.1:有过期时间的锁 锁的等待时间3秒，过期时间5秒
+            lockResult = distributionLockHandler.tryLock(lockKey, 3, 5);
+            // 3.2:带时间单位的锁 锁的等待时间3000毫秒，过期时间5000毫秒
+            lockResult = distributionLockHandler.tryLock(lockKey, 3000, 5000, TimeUnit.MILLISECONDS);
+            // --- 级联锁 ---
+            // 3.3:尝试非公平级联锁 有过期时间的锁 锁的等待时间3秒，过期时间5秒
+            lockResult = distributionLockHandler.tryLock(lockKeys, 3, 5);
+            // 3.2:带时间单位的锁 锁的等待时间3000毫秒，过期时间5000毫秒
+            lockResult = distributionLockHandler.tryLock(lockKeys, 3000, 5000, TimeUnit.MILLISECONDS);
+
+            // 4:尝试公平锁
+            // 3.1:有过期时间的锁 锁的等待时间3秒，过期时间5秒
+            lockResult = distributionLockHandler.tryFairLock(lockKey, 3, 5);
+            // 3.2:带时间单位的锁 锁的等待时间3000毫秒，过期时间5000毫秒
+            lockResult = distributionLockHandler.tryFairLock(lockKey, 3000, 5000, TimeUnit.MILLISECONDS);
+
+            return request.execute();
+        } finally {
+            distributionLockHandler.unlock(lockResult.getLock());
+        }
+    }
 
 
 }
