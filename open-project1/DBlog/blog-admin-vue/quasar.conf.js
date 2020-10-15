5076a8174841c7e466f27a4161fc823017080267
sikadai <466608943@qq.com>
2020-10-15 21:28:15 +0800

    支持动画

--- open-project1/DBlog/blog-admin-vue/quasar.conf.js
+++ open-project1/DBlog/blog-admin-vue/quasar.conf.js
@@ -142,10 +142,7 @@ module.exports = function (ctx) {
 
     // animations: 'all', // --- includes all animations
     // https://quasar.dev/options/animations
-    animations: [
-      'bounceInLeft',
-      'bounceOutRight'
-    ],
+    animations: 'all',
 
     // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
