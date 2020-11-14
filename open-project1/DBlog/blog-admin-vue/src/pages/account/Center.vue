892c19bcdd1d388b8e4f8e84f5205d09cfc065d3
sikacode <466608943@qq.com>
2020-11-15 00:28:10 +0800

    完善個人中心

--- open-project1/DBlog/blog-admin-vue/src/pages/account/Center.vue
+++ open-project1/DBlog/blog-admin-vue/src/pages/account/Center.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-md-4 col-xs-12 q-mb-md bg-grey-2" :class="{'q-pr-md': $q.screen.gt.xs}">
+    <div class="col-md-4 col-xs-12" :class="{'q-pr-md': $q.screen.gt.xs}">
       <q-card flat class="bg-white no-border-radius">
         <q-card-section class="text-center">
           <q-img src="~assets/head.png" style="max-width: 140px"></q-img>
@@ -21,13 +21,15 @@
             </q-item-label>
           </div>
         </q-card-section>
-        <q-separator inset=""/>
+        <q-separator inset="" />
         <q-card-section>
           <q-item-label class="text-body1">标签</q-item-label>
           <q-item-label class="text-body1 q-gutter-sm">
-            <q-chip size="sm" :selected.sync="desert.Cupcake" removable v-model="icecream" color="primary" text-color="white" icon="event" label="很有想法的" square/>
+            <q-chip size="sm" :selected.sync="desert.Cupcake" removable v-model="icecream" color="primary"
+                    text-color="white" icon="event" label="很有想法的" square />
             <q-chip size="sm" color="teal" text-color="white" icon="bookmark" label="专注设计" />
-            <q-chip size="sm" removable v-model="eclair" label="辣~" class="glossy" color="orange" text-color="white" icon-right="star"/>
+            <q-chip size="sm" removable v-model="eclair" label="辣~" class="glossy" color="orange" text-color="white"
+                    icon-right="star" />
             <q-chip size="sm" color="deep-orange" text-color="white" icon="directions" label="大长腿" />
             <q-chip size="sm">
               <q-avatar icon="bookmark" color="red" text-color="white" />
@@ -41,60 +43,61 @@
               <q-avatar color="red" text-color="white">50</q-avatar>
               海纳百川2
             </q-chip>
-            <q-chip size="sm" :selected.sync="desert.Icecream" color="primary" text-color="white" class="glossy" icon="event">
+            <q-chip size="sm" :selected.sync="desert.Icecream" color="primary" text-color="white" class="glossy"
+                    icon="event">
               海纳百川3
             </q-chip>
             <q-chip size="sm" removable v-model="gingerbread" color="info" text-color="white">
               <q-avatar>
-                <q-img src="https://cdn.quasar.dev/img/avatar5.jpg"/>
+                <q-img src="https://cdn.quasar.dev/img/avatar5.jpg" />
               </q-avatar>
               John
             </q-chip>
           </q-item-label>
         </q-card-section>
-        <q-separator inset=""/>
+        <q-separator inset="" />
         <q-card-section>
           <q-item-label class="text-body1">团队</q-item-label>
           <q-item-label class="q-gutter-sm">
             <q-chip size="16px" color="white">
               <q-avatar size="sm">
-                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"/>
+                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png" />
               </q-avatar>
               科学搬砖组
             </q-chip>
             <q-chip size="16px" color="white">
               <q-avatar size="sm">
-                <q-img src="https://cdn.quasar.dev/img/avatar5.jpg"/>
+                <q-img src="https://cdn.quasar.dev/img/avatar5.jpg" />
               </q-avatar>
               科学搬砖组
             </q-chip>
             <q-chip size="16px" color="white">
               <q-avatar size="sm">
-                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png"/>
+                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png" />
               </q-avatar>
               全组都是吴彦祖
             </q-chip>
             <q-chip size="16px" color="white">
               <q-avatar size="sm">
-                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png"/>
+                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png" />
               </q-avatar>
               中二少女团
             </q-chip>
             <q-chip size="16px" color="white">
               <q-avatar size="sm">
-                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png"/>
+                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png" />
               </q-avatar>
               程序员日常
             </q-chip>
             <q-chip size="16px" color="white">
               <q-avatar size="sm">
-                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png"/>
+                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png" />
               </q-avatar>
               高逼格设计天团
             </q-chip>
             <q-chip size="16px" color="white">
               <q-avatar size="sm">
-                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/kZzEzemZyKLKFsojXItE.png"/>
+                <q-img src="https://gw.alipayobjects.com/zos/rmsportal/kZzEzemZyKLKFsojXItE.png" />
               </q-avatar>
               骗你来学计算机
             </q-chip>
@@ -103,67 +106,186 @@
       </q-card>
     </div>
     <div class="col-md-8 col-xs-12">
-      <div class="row q-px-sm q-py-sm bg-white">
-        <div
-          class="col-xl-3 col-sm-4 col-xs-12"
-          v-bind:key="item.id"
-          v-for="item in items"
+        <q-tabs
+          v-model="tab"
+          class="bg-white"
+          active-color="primary"
+          indicator-color="primary"
+          align="left"
+          narrow-indicator
         >
-          <div
-            class="q-ma-sm cursor-pointer"
-            :class="{
-            'shadow-5 text-primary ': item.select,
-            'text-grey-8': !item.select
-          }"
-            @mouseover="select(item)"
-            @mouseleave="clear(item)"
-          >
-            <q-card square bordered flat>
-              <q-img :src="item.src" :ratio="16 / 10" />
-              <q-list>
-                <q-item clickable>
-                  <q-item-section avatar>
-                    <q-icon color="primary" name="local_bar" />
-                  </q-item-section>
-
+          <q-tab name="mails" label="文章(8)" />
+          <q-tab name="alarms" label="应用(8)" />
+          <q-tab name="movies" label="项目(8)" />
+        </q-tabs>
+        <q-separator />
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="mails" class="q-pa-sm">
+            <q-list class="q-py-sm" >
+              <q-intersection
+                once
+                v-for="index in 100"
+                :key="index"
+              >
+                <q-item clickabl>
                   <q-item-section>
-                    <q-item-label>Bar XYZ</q-item-label>
-                    <q-item-label caption>Have a drink.</q-item-label>
+                    <q-item-label class="text-body1 q-mb-sm">
+                      <strong>Alipay</strong>
+                    </q-item-label>
+                    <q-item-label class="q-mb-sm">
+                      <q-chip label="Sika Design" color="primary" text-color="white" square size="12px"></q-chip>
+                      <q-chip label="设计语言" color="info" text-color="white" square size="12px"></q-chip>
+                      <q-chip label="蚂蚁金服" color="deep-orange" text-color="white" square size="12px"></q-chip>
+                    </q-item-label>
+                    <q-item-label class="text-body1 text-grey-8 q-mb-sm">
+                      段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。
+                    </q-item-label>
+                    <q-item-label class="row text-body1 text-grey-7 items-center q-gutter-x-xs">
+                      <q-chip color="white" text-color="primary" class="col-auto q-pa-sm cursor-pointer">
+                        <q-avatar size="18px">
+                          <q-img src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png" />
+                        </q-avatar>
+                        姜宇
+                      </q-chip>
+                      <span class="col-auto">发布在</span>
+                      <span class="col-auto text-primary cursor-pointer">https://ant.design</span>
+                      <span class="col-sm-auto col-xs-12">2020-11-14 16:15</span>
+                    </q-item-label>
+                    <q-item class="q-px-none">
+                      <q-btn flat round dense icon="star_border" label="118" size="12px"  class="q-mr-sm" color="primary" />
+                      <q-separator vertical inset="" />
+                      <q-btn flat round dense icon="thumb_up" label="118" size="12px" color="grey-6" class="q-mx-sm" />
+                      <q-separator vertical inset="" />
+                      <q-btn flat round dense icon="message" label="118" size="12px" color="grey-6" class="q-mx-sm" />
+                    </q-item>
                   </q-item-section>
                 </q-item>
+                <q-separator inset="" spaced="15px"/>
+              </q-intersection>
+            </q-list>
+          </q-tab-panel>
+          <q-tab-panel name="alarms">
+            <div class="row">
+              <div
+                class="col-xl-3 col-sm-4 col-xs-12"
+                v-bind:key="item.id"
+                v-for="item in items"
+              >
+                <q-intersection
+                  once
+                  class="q-ma-sm cursor-pointer"
+                  :class="{
+                  'shadow-blue text-primary ': item.select,
+                  'text-grey-8': !item.select}"
+                  @mouseover="select(item)"
+                  @mouseleave="clear(item)"
+                >
+                  <q-card square bordered flat>
+                    <q-img :src="item.src" :ratio="16 / 10" />
+                    <q-list>
+                      <q-item clickable>
+                        <q-item-section avatar>
+                          <q-icon color="primary" name="local_bar" />
+                        </q-item-section>
 
-                <q-item clickable>
-                  <q-item-section avatar>
-                    <q-icon color="red" name="local_gas_station" />
-                  </q-item-section>
+                        <q-item-section>
+                          <q-item-label>Bar XYZ</q-item-label>
+                          <q-item-label caption>Have a drink.</q-item-label>
+                        </q-item-section>
+                      </q-item>
 
-                  <q-item-section>
-                    <q-item-label>Gas Station</q-item-label>
-                    <q-item-label caption>Fill your gas tank.</q-item-label>
-                  </q-item-section>
-                </q-item>
+                      <q-item clickable>
+                        <q-item-section avatar>
+                          <q-icon color="red" name="local_gas_station" />
+                        </q-item-section>
 
-                <q-item clickable>
-                  <q-item-section avatar>
-                    <q-icon color="amber" name="local_movies" />
-                  </q-item-section>
+                        <q-item-section>
+                          <q-item-label>Gas Station</q-item-label>
+                          <q-item-label caption>Fill your gas tank.</q-item-label>
+                        </q-item-section>
+                      </q-item>
 
-                  <q-item-section>
-                    <q-item-label>Cinema XYZ</q-item-label>
-                    <q-item-label caption>Watch a movie.</q-item-label>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-card>
-          </div>
-        </div>
-      </div>
+                      <q-item clickable>
+                        <q-item-section avatar>
+                          <q-icon color="amber" name="local_movies" />
+                        </q-item-section>
+
+                        <q-item-section>
+                          <q-item-label>Cinema XYZ</q-item-label>
+                          <q-item-label caption>Watch a movie.</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-card>
+                </q-intersection>
+              </div>
+            </div>
+          </q-tab-panel>
+          <q-tab-panel name="movies">
+            <div class="row">
+              <div
+                class="col-xl-3 col-sm-4 col-xs-12"
+                v-bind:key="item.id"
+                v-for="item in items"
+              >
+                <q-intersection
+                  once
+                  class="q-ma-sm cursor-pointer"
+                  :class="{
+                  'shadow-blue text-primary ': item.select,
+                  'text-grey-8': !item.select}"
+                  @mouseover="select(item)"
+                  @mouseleave="clear(item)"
+                >
+                  <q-card square bordered flat>
+                    <q-img :src="item.src" :ratio="16 / 10" />
+                    <q-list>
+                      <q-item clickable>
+                        <q-item-section avatar>
+                          <q-icon color="primary" name="local_bar" />
+                        </q-item-section>
+
+                        <q-item-section>
+                          <q-item-label>Bar XYZ</q-item-label>
+                          <q-item-label caption>Have a drink.</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable>
+                        <q-item-section avatar>
+                          <q-icon color="red" name="local_gas_station" />
+                        </q-item-section>
+
+                        <q-item-section>
+                          <q-item-label>Gas Station</q-item-label>
+                          <q-item-label caption>Fill your gas tank.</q-item-label>
+                        </q-item-section>
+                      </q-item>
+
+                      <q-item clickable>
+                        <q-item-section avatar>
+                          <q-icon color="amber" name="local_movies" />
+                        </q-item-section>
+
+                        <q-item-section>
+                          <q-item-label>Cinema XYZ</q-item-label>
+                          <q-item-label caption>Watch a movie.</q-item-label>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-card>
+                </q-intersection>
+              </div>
+            </div>
+          </q-tab-panel>
+        </q-tab-panels>
     </div>
   </div>
 </template>
 
 <script>
 import _ from 'lodash'
+
 const types = []
 types.push('全部')
 for (let i = 0; i < 11; ++i) {
@@ -220,7 +342,7 @@ for (let i = 0; i < 30; ++i) {
 }
 export default {
   name: 'Center',
-  data () {
+  data() {
     return {
       icecream: true,
       eclair: true,
@@ -232,6 +354,7 @@ export default {
         Cupcake: false,
         Gingerbread: false
       },
+      tab: 'mails',
       types,
       owner: [],
       activeUser: '张三',
@@ -248,7 +371,7 @@ export default {
     }
   },
   computed: {
-    selection () {
+    selection() {
       return Object.keys(this.desert)
         .filter(type => this.desert[type] === true)
         .join(', ')
@@ -277,4 +400,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.shadow-blue {
+  box-shadow: 0 0 8px -1px #31CCEC;
+}
+</style>
