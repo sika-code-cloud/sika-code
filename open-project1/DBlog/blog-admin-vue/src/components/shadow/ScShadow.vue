0409db18da333764dffcc9a9765d43ea1b6fb064
sikadai <466608943@qq.com>
2020-11-27 23:10:10 +0800

    公共阴影组件

---
+++ open-project1/DBlog/blog-admin-vue/src/components/shadow/ScShadow.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cursor-pointer" :class="{'sc-shadow-blue text-primary ': active, 'text-grey-8': !active}"
+       @mouseover="select()"
+       @mouseleave="clear()">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScShadow',
+  data() {
+    return {
+      active: false
+    }
+  },
+  props: {
+  },
+  methods: {
+    select() {
+      this.active = true
+    },
+    clear() {
+      this.active = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
