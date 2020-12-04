28aba11d3443a732128428784dea9bc4af640cc6
sikadai <466608943@qq.com>
2020-12-04 22:35:37 +0800

    日期组件完成

--- open-project1/DBlog/blog-admin-vue/src/components/common/ScStartEndDate.vue
+++ open-project1/DBlog/blog-admin-vue/src/components/common/ScStartEndDate.vue
@@ -2,10 +2,10 @@
   <q-field :square="fieldSquare" :dense="fieldDense" outlined :style="fieldStyle">
     <template v-slot:control>
       <div class="self-center full-width no-outline" tabindex="0">
-        {{ formatStartAndEndDate }}
+        {{ startAndEndDateFormat }}
       </div>
     </template>
-    <template v-slot:append>
+    <template v-slot:prepend>
       <q-icon :name="dateIcon" class="cursor-pointer" :color="dateIconColor">
         <q-menu
           square
@@ -17,15 +17,31 @@
         </q-menu>
       </q-icon>
     </template>
+    <template v-slot:append>
+      <q-icon :name="clearIcon" class="cursor-pointer" :color="clearIconColor" v-on:click="clear()" />
+    </template>
   </q-field>
 </template>
 
 <script>
 import { date } from 'quasar'
-
+const startAndEndDateDefault = {
+  from: '',
+  to: ''
+}
 export default {
   name: 'ScStartEndDate',
   props: {
+    clearIcon: {
+      type: String,
+      default: 'cancel',
+      required: false
+    },
+    clearIconColor: {
+      type: String,
+      default: 'grey',
+      required: false
+    },
     dateFormat: {
       type: String,
       default: 'YYYY-MM-DD',
@@ -87,24 +103,42 @@ export default {
       startAndEndDate: {
         from: this.fromDate,
         to: this.toDate
-      }
+      },
+      startAndEndDateFormat: ''
     }
   },
   methods: {
     formatDate(dateFormat) {
       return date.formatDate(dateFormat, this.dateFormat)
+    },
+    formatStartAndAnd() {
+      this.startAndEndDateFormat = this.formatDate(this.startAndEndDate.from) + '~' + this.formatDate(this.startAndEndDate.to)
+    },
+    buildNow() {
+      return date.startOfDate(Date.now(), 'day')
+    },
+    clear() {
+      this.startAndEndDate = startAndEndDateDefault
     }
   },
-  computed: {
-    formatStartAndEndDate() {
-      if (!this.startAndEndDate) {
-        return ''
-      }
-      return (
-        this.formatDate(this.startAndEndDate.from) +
-        '~' +
-        this.formatDate(this.startAndEndDate.to)
-      )
+  mounted() {
+    this.formatStartAndAnd()
+  },
+  watch: {
+    startAndEndDate: {
+      handler(newValue, oldValue) {
+        if (!newValue) {
+          newValue = startAndEndDateDefault
+          this.startAndEndDate = startAndEndDateDefault
+        }
+        if (!newValue || newValue.from === '' || newValue.to === '') {
+          this.startAndEndDateFormat = ''
+          return
+        }
+        this.formatStartAndAnd()
+      },
+      immediate: true,
+      deep: true
     }
   }
 }
