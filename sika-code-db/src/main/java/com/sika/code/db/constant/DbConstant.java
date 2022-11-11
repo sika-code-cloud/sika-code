9eb700ac13af42bd06a08ced00c5d9aa2f92c09b
sikadai <sikacode@163.com>
2022-11-11 11:06:44 +0800

    feat: 添加常量类+分片更新

---
+++ sika-code-db/src/main/java/com/sika/code/db/constant/DbConstant.java
@@ -0,0 +1,17 @@
+package com.sika.code.db.constant;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author by sikadai
+ * @version 1.0
+ * @since 2022/11/11 10:52
+ */
+public class DbConstant {
+
+    public static final String SET_VERSION_SQL = "version = version + 1";
+    public static final String LOGIC_DELETED_SQL = "is_deleted = 1";
+    public static final String IS_DELETED_KEY = "is_deleted";
+}
